a31ee566715ce3505ecf60e8d1d13dd2a43dbdeb
Jim Kim <icekjm1029@gmail.com>
2025-04-19 00:35:52 +0900

    feat:principle of params lazy evaluation

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -9,8 +9,6 @@ export function generateStaticParams() {
   return [{ id: "1" }, { id: "2" }, { id: "3" }]; //주의사항: id값을 문자열로만 써야함
 }
 
-//export const dynamic = "force-dynamic";
-
 async function BookDetail({ bookId }: { bookId: string }) {
   //GenerateStaticParams라는 함수를 내보내 주시게 되면 페이지 컴퍼넌트 내부에
   // 데이터 캐싱이 설정되지 않은 이러한 데이터 페칭인 fetch가 존재하게 될지라도 무조건 해당하는
@@ -77,10 +75,40 @@ function ReviewEditor() {
 //params는 Promise 객체임
 //함수나 컴포넌트, 혹은 어떤 변수의 값이 "나중에 비동기적으로 도착"할 예정이라면 그 타입은 Promise<T>로 정의
 //이 Promise가 나중에 { id: string | string[] }라는 객체 하나를 반환함 -> 이 객체 안에 있는 id라는 키의 값은 문자열(string) 혹은 문자열배열임(string{})
-export default function Page({ params }: { params: { id: string } }) {
+
+//아래 코드는 Next.js 15 이전버전에서는 잘되었으나, 15부터 params와 searchParams는 지연평가되어 아래처럼 처리하면안됨
+// export default function Page({ params }: { params: { id: string } }) {
+//   return (
+//     <div className={style.container}>
+//       <BookDetail bookId={params.id} />
+//       <ReviewEditor />
+//     </div>
+//   );
+// }
+
+// 해결책1
+// export default async function Page(props: { params: Promise<{ id: string }> }) {
+//   const { id } = await props.params;
+
+//   return (
+//     <div className={style.container}>
+//       <BookDetail bookId={id} />
+//       <ReviewEditor />
+//     </div>
+//   );
+// }
+
+// 해결책2
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+
   return (
     <div className={style.container}>
-      <BookDetail bookId={params.id} />
+      <BookDetail bookId={id} />
       <ReviewEditor />
     </div>
   );
