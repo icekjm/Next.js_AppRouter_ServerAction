cebdd419cdc4cf2f533f849894db3a568ae151b2
Jim Kim <icekjm1029@gmail.com>
2025-04-24 00:27:11 +0900

    feat:revalidatePath added

--- src/actions/create-review.action.ts
+++ src/actions/create-review.action.ts
@@ -1,5 +1,7 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
+
 export async function createReviewAction(formData: FormData) {
   const bookId = formData.get("bookId")?.toString();
   const content = formData.get("content")?.toString();
@@ -18,6 +20,9 @@ export async function createReviewAction(formData: FormData) {
       }
     );
     console.log(response.status);
+    //넥스트서버측에게 페이지를 다시 생성하도록 요청해서 서버액션의 결과를 바로 화면에 나타나게함
+    //오직 서버측에서만 호출함 revalidatePath -> 해당 페이지를 재생성하면서 데이터캐싱이 force-cache더라도 다 삭제됨, 풀라우트 캐시도 삭제됨
+    revalidatePath(`/book/${bookId}`);
   } catch (err) {
     console.log(err);
     return;
