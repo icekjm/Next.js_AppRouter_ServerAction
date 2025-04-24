7768b8450d66ff4b4c14b5c4784c7fc7a01f2eca
Jim Kim <icekjm1029@gmail.com>
2025-04-24 21:44:04 +0900

    feat:various revalidatePath

--- src/actions/create-review.action.ts
+++ src/actions/create-review.action.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { revalidatePath } from "next/cache";
+import { revalidatePath, revalidateTag } from "next/cache";
 
 export async function createReviewAction(formData: FormData) {
   const bookId = formData.get("bookId")?.toString();
@@ -22,7 +22,22 @@ export async function createReviewAction(formData: FormData) {
     console.log(response.status);
     //넥스트서버측에게 페이지를 다시 생성하도록 요청해서 서버액션의 결과를 바로 화면에 나타나게함
     //오직 서버측에서만 호출함 revalidatePath -> 해당 페이지를 재생성하면서 데이터캐싱이 force-cache더라도 다 삭제됨, 풀라우트 캐시도 삭제됨
-    revalidatePath(`/book/${bookId}`);
+
+    //1. 특정주소의 해당하는 페이지만 재검증
+    // revalidatePath(`/book/${bookId}`);
+
+    //2. 특정 경로의 모든 동적 페이지를 재검증
+    // revalidatePath("/book/[id]", "page");
+
+    // 3. 특정 레이아웃을 갖는 모든 페이지 재검증
+    // revalidatePath("/(with-searchbar)", "layout");
+
+    // 4. 인덱스 경로에 있는 레이아웃인 루트 레트레이아웃을 갖는 모든 페이지들이 전부 다 재검증이 됨
+    // revalidatePath("/", "layout");
+
+    //5. 태그 기준, 데이터 캐시 재검증 -> fetch를 통해 데이터를 가져올때 아래 태그를 갖는 데이터캐시만 삭제하고 이를 재검증
+    // 이 방법은 1번과 비교해야함. 1번은 특정경로의 페이지에 해당하는 모든 데이터캐시를 삭제함
+    revalidateTag(`review-${bookId}`);
   } catch (err) {
     console.log(err);
     return;
--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -79,7 +79,9 @@ async function BookDetail({ bookId }: { bookId: string }) {
 
 async function ReviewList({ bookId }: { bookId: string }) {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review/book/${bookId}`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review/book/${bookId}
+    `
+    // { next: { tags: [`review-${bookId}`] } } //create-review.action.tsx에서 revalidate와 관련
   );
 
   if (!response.ok) {
